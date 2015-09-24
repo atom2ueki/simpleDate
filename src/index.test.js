11185a9b5e08aac66a1499cc1d387f2b057a9bf1
Tony Li <atom2ueki@gmail.com>
2015-09-24 23:11:32 +0800

    add mocha chai test

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var expect = require('chai').expect;
+var simpleDate = require('./simpleDate');
+
+describe('simpleDate-func', function(){
+  it('should work!', function(){
+    expect(true).to.be.true;
+  });
+});
